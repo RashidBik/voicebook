3f6a026a2d46d34badbe945879fd14364ffa5f0e
rashidbik <abdurashid.bigzad1999@gmail.com>
2023-07-29 14:47:37 +0430

    table

--- src/routes/pages/lock/+page.svelte
+++ src/routes/pages/lock/+page.svelte
@@ -1,2 +1,15 @@
-<h1>lock</h1>
-
+<div>
+    <div class="p-4 text-center">
+        <p>ACTIVE GROUP <small><i>all</i></small></p>
+    </div>
+    <div class="w-full flex flex-col">
+    <div class="w-full flex p-1">
+        <div class="w-[60%] h-24 p-2 bg-slate-400">all 🌎</div>
+        <div class="w-[40%] h-24 p-2 bg-yellow-400">music</div>
+    </div>
+    <div class="w-full flex p-1">
+        <div class="w-[40%] h-24 p-2 bg-lime-400">story</div>
+        <div class="w-[60%] h-24 p-2 bg-pink-400">voice</div>
+    </div>
+    </div>
+</div>
--- src/routes/pages/table/+page.svelte
+++ src/routes/pages/table/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Table</h1>
+<script>
 
+</script>
+<div>
+    <div>
+        music playing 🎵
+    </div>
+    <div class="grid grid-cols-3 w-full justify-stretch items-stretch place-items-center  ">
+        <div class="border my-1 w-[80%]">
+            <div class="border w-18 h-14 flex justify-center items-center text-xl">🎼</div>
+            <div class="flex justify-around w-full h-3 text-xs">
+                <p>Unknown</p>
+                <span class="">🧑‍🦱</span>
+            </div>
+            <div class="w-full h-3 text-[10px]">some text as </div>
+        </div>
+                <div class="border my-1 w-[80%]">
+            <div class="border w-18 h-14 flex justify-center items-center text-xl">🎼</div>
+            <div class="flex justify-around w-full h-3 text-xs">
+                <p>Unknown</p>
+                <span class="">🧑‍🦱</span>
+            </div>
+            <div class="w-full h-3 text-[10px]">some text as </div>
+        </div>
+                <div class="border my-1 w-[80%]">
+            <div class="border w-18 h-14 flex justify-center items-center text-xl">🎼</div>
+            <div class="flex justify-around w-full h-3 text-xs">
+                <p>Unknown</p>
+                <span class="">🧑‍🦱</span>
+            </div>
+            <div class="w-full h-3 text-[10px]">some text as </div>
+        </div>
+                <div class="border my-1 w-[80%]">
+            <div class="border w-18 h-14 flex justify-center items-center text-xl">🎼</div>
+            <div class="flex justify-around w-full h-3 text-xs">
+                <p>Unknown</p>
+                <span class="">🧑‍🦱</span>
+            </div>
+            <div class="w-full h-3 text-[10px]">some text as </div>
+        </div>
+                <div class="border my-1 w-[80%]">
+            <div class="border w-18 h-14 flex justify-center items-center text-xl">🎼</div>
+            <div class="flex justify-around w-full h-3 text-xs">
+                <p>Unknown</p>
+                <span class="">🧑‍🦱</span>
+            </div>
+            <div class="w-full h-3 text-[10px]">some text as </div>
+        </div>
+    </div>
+</div>
